bb3dccd59b8cd812bba1b804215160114830a8a3
dan <44970412+dtbe@users.noreply.github.com>
2024-12-01 22:21:13 +0000

    x

--- app/crisis-monitor/page.tsx
+++
@@ -1,9 +0,0 @@
-import { QuestGenerator } from '@/components/quest-generator'
-
-export default function CrisisMonitorPage() {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-purple-900 to-blue-900 flex items-center justify-center p-4">
-      <QuestGenerator />
-    </div>
-  )
-}
